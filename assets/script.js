826e0f84620e5cfa03647936298631ae34883eae
James Xiong <jxiong783@gmail.com>
2023-02-01 22:57:28 -0800

    created variables for the html components

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
 async function displayForecast(city) {
     const API_KEY = '481eb4fe101dd3b7b6c1dd83891ed073';
     const API_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+    
+    try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+
+        const forecastData = data.list.filter((weatherData, index) => index % 8 === 0);
+        const forecastContainer = document.querySelector('#forecast-container');
+        const forecastCard = document.createElement('div');
+        const dateElement = document.createElement('p');
+        const temperatureElement = document.createElement('p');
+        const humidityElement = document.createElement('p');
+        const windSpeedElement = document.createElement('p');
+        const iconElement = document.createElement('img');
